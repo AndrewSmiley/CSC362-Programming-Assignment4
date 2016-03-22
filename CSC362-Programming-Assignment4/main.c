db290ab540c577f141d33ddb2d4f98c1bd575d51
Andrew Smiley <andrew.smiley@live.com>
2016-03-22 11:11:47 -0400

    Initial Commit

---
+++ CSC362-Programming-Assignment4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSC362-Programming-Assignment4
+//
+//  Created by Andrew Smiley on 3/22/16.
+//  Copyright © 2016 Andrew Smiley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
